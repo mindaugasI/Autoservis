dbaff71e13d4a275f62a0f0c14aa4a9d52949922
Mindaugas Ivanauskas <mindaugas.ivanauskas@gmail.com>
2023-04-29 11:08:23 +0300

    feat: add custom admin page views for OrderList

--- carservice/admin.py
+++ carservice/admin.py
@@ -1,15 +1,31 @@
 from django.contrib import admin
-from .models import CarModel, Car, Service, ServicePrice, Order
+from .models import CarModel, Car, Service, ServicePrice, Order, OrderList
+
+
+class OrderInline(admin.TabularInline):
+    model = Order
+    # Turn off extra empty lines for input
+    extra = 0
+
+
+class OrderListAdmin(admin.ModelAdmin):
+    list_display = ('car', 'order_date')
+    inlines = [OrderInline]
+
+
 
 class CarAdmin(admin.ModelAdmin):
     list_display = ('client', 'plate_no', 'car_model', 'vin_number')
     list_filter = ('client', 'car_model')
     search_fields = ('plate_no', 'vin_number')
 
+class ServicePriceAdmin(admin.ModelAdmin):
+    list_display = ('service', 'price')
+
 # Register your models here.
 admin.site.register(CarModel)
 admin.site.register(Car, CarAdmin)
 admin.site.register(Service)
 admin.site.register(ServicePrice)
+admin.site.register(OrderList, OrderListAdmin)
 admin.site.register(Order)
-#admin.site.register(OrderList)
