657ce0902e2a4498d8502d13da953ddf7d899254
Mindaugas Ivanauskas <mindaugas.ivanauskas@gmail.com>
2023-05-04 18:01:17 +0300

    add templates, modified models, views

---
+++ carservice/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from . import views
+
+
+urlpatterns = [
+    # Index
+    path('', views.index, name='index'),
+    # Page where all cars are displayed
+    path('cars/', views.cars, name='cars'),
+    path('cars/<int:car_id>', views.specific_car, name='specific_car'),
+    path('services/', views.services, name='services'),
+    path('orders/', views.orders, name='orders'),
+    path('orders/<int:order_id>/', views.specific_order, name='specific_order')
+]
--- carservice/views.py
+++ carservice/views.py
@@ -1,3 +1,47 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import *
 
-# Create your views here.
+
+def index(request):
+    car_models = CarModel.objects.all()
+    car_model_count = CarModel.objects.count()
+    services = Service.objects.all()
+    services_count = Service.objects.count()
+
+    context = {
+        'car_models': car_models,
+        'car_model_count': car_model_count,
+        'services': services,
+        'services_count': services_count,
+    }
+    return render(request, "index.html", context)
+
+def cars(request):
+    all_cars = CarModel.objects.all()
+    context = {
+        'cars': all_cars
+    }
+    return render(request, "cars.html", context)
+
+def specific_car(request, car_id):
+    car = Car.objects.get(car_id=car_id)
+    context = {'car': car}
+    return render(request, "specific_car.html", context)
+
+
+def services(request):
+    all_services = Service.objects.all()
+    context = {
+        'services': all_services
+    }
+    return render(request, "services.html", context)
+
+def orders(request):
+    all_orders = OrderList.objects.all()
+    context = {
+        'orders': all_orders
+    }
+    return render(request, 'orders.html', context)
+
+def specific_order(request, order_id):
+    pass
