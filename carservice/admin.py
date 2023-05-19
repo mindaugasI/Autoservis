ecac0ff07527a9e648ab03324156647e043ec635
Mindaugas Ivanauskas <mindaugas.ivanauskas@gmail.com>
2023-05-19 22:05:08 +0300

    add: search, car photos. Photos connected to specifik orders. Search engine: owner, model, plate no, vin no.

--- carservice/admin.py
+++ carservice/admin.py
@@ -1,29 +1,17 @@
 from django.contrib import admin
 from .models import CarModel, Car, Service, ServicePrice, Order, OrderList
 
-<<<<<<< HEAD
+
 class OrderInline(admin.TabularInline):
     model = Order
     # turn off extra empty lines for inputs
     extra = 0
 
-class OrderListAdmin(admin.ModelAdmin):
-    list_display = ('car', 'order_date')
-    inlines = [OrderInline]
-=======
-
-class OrderInline(admin.TabularInline):
-    model = Order
-    # Turn off extra empty lines for input
-    extra = 0
-
 
 class OrderListAdmin(admin.ModelAdmin):
     list_display = ('car', 'order_date')
     inlines = [OrderInline]
 
-
->>>>>>> 7211486ff028c2355969fff4557358f39cd6a75b
 
 class CarAdmin(admin.ModelAdmin):
     list_display = ('client', 'plate_no', 'car_model', 'vin_number')
